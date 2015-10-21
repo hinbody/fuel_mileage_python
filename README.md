de4426223d8ff32ccf707f0d9db6483abb25e044
Harley Inbody <hinbody@gmail.com>
2015-10-20 21:16:54 -0400

    Adds current mileage, gallons, and price_per_gallon in one function store_current_data()

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Functions needed written
 * store\_current\_data()
   - store the current mileage of the vehicle **Done**
-  - store the current number of gallons to fill
- - store the current price per gallon paid
+  - store the current number of gallons to fill **Done**
+ - store the current price per gallon paid **Done**
 * calculate\_mpg()
  - calculate the miles per gallon used since last fill up
 * create\_vehicle()
--- fuel_mileage.py
+++ fuel_mileage.py
@@ -1,7 +1,5 @@
 import db_config
 import sqlite3
-#db_name = 'fuel_mileage.db'
-#conn = sqlite3.connect(db_name)
 
 db_conn = db_config.cursor()
 try:
@@ -13,7 +11,8 @@ except Exception, e:
     print 'error =>', e
 
 
-def store_current_mileage(miles):
-    the_miles = (miles,)
-    db_conn.execute('INSERT INTO fuel (mileage) VALUES (?)', the_miles)
+def store_current_data(miles, gallons, price_per_gallon):
+    the_miles = (miles, gallons, price_per_gallon)
+    db_conn.execute('''INSERT INTO fuel (mileage, gallons, pricepergallon)
+    VALUES (?, ?, ?)''', the_miles)
     db_config.commit()
